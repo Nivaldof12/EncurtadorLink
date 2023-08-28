98247f31a96f0edd0ea5dd5c60d3a5fe9bffa346
Nivaldo <nivaldopvp@gmail.com>
2023-08-28 16:31:59 -0300

    Configurando Pom e app.properties

---
+++ urlencurtador/src/main/java/br/com/urlencurtador/UrlencurtadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.urlencurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlencurtadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlencurtadorApplication.class, args);
+	}
+
+}
---
+++ urlencurtador/src/test/java/br/com/urlencurtador/UrlencurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urlencurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlencurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
