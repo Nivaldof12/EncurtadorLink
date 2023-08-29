e2d690512451fde7a61c8262647ce8cbc86aa79d
Nivaldo <nivaldopvp@gmail.com>
2023-08-29 15:16:28 -0300

    Update Service

--- urlencurtador/src/main/java/br/com/urlencurtador/service/UrlService.java
+++ urlencurtador/src/main/java/br/com/urlencurtador/service/UrlService.java
@@ -1,8 +1,28 @@
 package br.com.urlencurtador.service;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.urlencurtador.domain.Url;
+import br.com.urlencurtador.repository.UrlRepository;
 
 @Service
 public class UrlService {
 
+	@Autowired
+	private UrlRepository urlRepository;
+
+	public Url incluir(Url url) {
+		return urlRepository.save(url);
+	}
+	
+	public void excluir(Long id) {
+		urlRepository.deleteById(id);
+	}
+	
+	public Collection<Url> obterLista(){
+		return (Collection<Url>) urlRepository.findAll();
+	}
 }
