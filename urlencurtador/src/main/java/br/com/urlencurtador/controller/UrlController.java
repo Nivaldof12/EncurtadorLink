a71f832ab756e12f2a48e7fabb89f804c41bed98
Nivaldo <nivaldopvp@gmail.com>
2023-08-29 15:16:41 -0300

    Update controller

--- urlencurtador/src/main/java/br/com/urlencurtador/controller/UrlController.java
+++ urlencurtador/src/main/java/br/com/urlencurtador/controller/UrlController.java
@@ -1,8 +1,43 @@
 package br.com.urlencurtador.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.urlencurtador.domain.Url;
+import br.com.urlencurtador.service.UrlService;
 
 @Controller
 public class UrlController {
  
+	
+	@Autowired
+	private UrlService urlService;
+
+	@GetMapping(value = "/")
+	public String telaIndex(Model model) {
+		
+		model.addAttribute("urls", urlService.obterLista());
+		
+		return "index";
+	}	
+	
+	@PostMapping(value = "/url/incluir")
+	public String incluir(Url url) {
+		
+		urlService.incluir(url);
+		
+		return "redirect:/";
+	}
+		
+	@GetMapping(value = "/url/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+
+		urlService.excluir(id);
+
+		return "redirect:/";
+	}
 }
